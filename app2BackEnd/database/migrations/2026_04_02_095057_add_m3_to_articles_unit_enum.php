897010a8a05f59a673ce0c8ac51a319330c88425
mostaphaxs <mostaphaff123@gmail.com>
2026-04-02 09:53:13 +0100

    als

--- app2BackEnd/database/migrations/2026_04_02_095057_add_m3_to_articles_unit_enum.php
+++ app2BackEnd/database/migrations/2026_04_02_095057_add_m3_to_articles_unit_enum.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,9 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('articles', function (Blueprint $table) {
-            //
-        });
+        DB::statement("ALTER TABLE articles MODIFY COLUMN unit ENUM('m2', 'ML', 'U', 'Kg', 'M3')");
     }
 
     /**
@@ -21,8 +20,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('articles', function (Blueprint $table) {
-            //
-        });
+        DB::statement("ALTER TABLE articles MODIFY COLUMN unit ENUM('m2', 'ML', 'U', 'Kg')");
     }
 };
