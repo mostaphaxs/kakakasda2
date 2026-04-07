dd5778c666debbcfa090a287564bfc08ad4282d8
mostaphaxs <mostaphaff123@gmail.com>
2026-04-07 17:14:39 +0100

    s

--- app2BackEnd/config/cors.php
+++ app2BackEnd/config/cors.php
@@ -1,13 +1,19 @@
-
 <?php
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'tauri://localhost',
+        'http://tauri.localhost',
+        'http://localhost:5173', // For Vite Dev
+    ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^tauri://.*$#',
+    ],
 
     'allowed_headers' => ['*'],
 
@@ -17,5 +23,3 @@ return [
 
     'supports_credentials' => true,
 ];
-
-?>
